fb9404720a5866ff4fd9c318b91fd935b3141736
Bbyz <bbyzdev@gmail.com>
2024-05-09 15:54:13 +0200

    Added cooldown to the Gnome abstract class

--- core/src/com/gdx/gnomearena/Core/Gnome.java
+++ core/src/com/gdx/gnomearena/Core/Gnome.java
@@ -7,27 +7,35 @@ public abstract class Gnome extends Entity
     GnomeMovePattern move;
     GnomeAttackPattern attack;
 
+    int moveCooldown;
+    int currentCooldown;
+
     public void makeMove(Board b)
     {
-        if(health==0)
+        currentCooldown--;
+        if(currentCooldown==0)
         {
-            onDeath();
-            return;
-        }
-
-        int gX = b.getEntitiesPosition(this).getKey();
-        int gY = b.getEntitiesPosition(this).getKey();
-
-        for(int i=0; i<attack.attackRange.length; i++)
-        {
-            if(b.getPlayersPosition() == new Pair<Integer,Integer>(gX + attack.attackRange[i].getKey(), gY + attack.attackRange[i].getValue()))
+            currentCooldown = moveCooldown;
+            if(health==0)
             {
-                attack.attack(b, new Pair<Integer,Integer>(gX, gY), b.getPlayersPosition());
+                onDeath();
                 return;
             }
-        }
-        
-        move.move(b, new Pair<Integer,Integer>(gX, gY));
+
+            int gX = b.getEntitiesPosition(this).getKey();
+            int gY = b.getEntitiesPosition(this).getKey();
+
+            for(int i=0; i<attack.attackRange.length; i++)
+            {
+                if(b.getPlayersPosition() == new Pair<Integer,Integer>(gX + attack.attackRange[i].getKey(), gY + attack.attackRange[i].getValue()))
+                {
+                    attack.attack(b, new Pair<Integer,Integer>(gX, gY), b.getPlayersPosition());
+                    return;
+                }
+            }
+            
+            move.move(b, new Pair<Integer,Integer>(gX, gY));
+            }
     }   
     public void onSpawn(){}
     public void onDeath(){}
