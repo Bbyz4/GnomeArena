ecfa13f34c3cbc39304fa628418a5fd008b01447
Bbyz <bbyzdev@gmail.com>
2024-05-11 22:00:55 +0200

    Added board rendering

--- core/src/com/gdx/gnomearena/Screens/GameScreen.java
+++ core/src/com/gdx/gnomearena/Screens/GameScreen.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.IntSet;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.gdx.gnomearena.MainGame;
@@ -17,6 +20,9 @@ public class GameScreen implements Screen {
     final MainGame game;
     final GameManager gameManager;
     private Stage stage;
+
+
+    private Image grassBlocks[][];
     
     private final float pace = 1f;
     private final float clickWindow = 0.25f;
@@ -41,6 +47,8 @@ public class GameScreen implements Screen {
             Input.Keys.D,
             Input.Keys.E
         );
+
+        grassBlocks = new Image[15][15];
     }
 
     @Override
@@ -60,6 +68,21 @@ public class GameScreen implements Screen {
                 return true;
             }
         });
+
+        Texture grass1 = new Texture("terrainSprites/Grass1.png");
+        Texture grass2 = new Texture("terrainSprites/Grass2.png");
+
+        for(int i=0; i<15; i++)
+        {
+            for(int j=0; j<15; j++)
+            {
+                Texture tex = (i+j)%2==0 ? grass2 : grass1;
+                Sprite sprite = new Sprite(tex);
+                grassBlocks[i][j] = new Image(sprite);
+                grassBlocks[i][j].setPosition(0+64*i, 0+64*j);
+                grassBlocks[i][j].setScale(2f);
+            }
+        }
     }
 
     @Override
@@ -83,18 +106,27 @@ public class GameScreen implements Screen {
         {
             if(!keyHandled)
             {
-                Gdx.gl.glClearColor(1,0,0,1);
+                //Gdx.gl.glClearColor(1,0,0,1);
             }
             else
             {
-                Gdx.gl.glClearColor(0,1,0,1);
+                //Gdx.gl.glClearColor(0,1,0,1);
             }
         }
         else
         {
-            Gdx.gl.glClearColor(0,0,0,1);
+            //Gdx.gl.glClearColor(0,0,0,1);
         }
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+
+        for(int i=0; i<15; i++)
+        {
+            for(int j=0; j<15; j++)
+            {
+                stage.addActor(grassBlocks[i][j]);
+            }
+        }
 
         stage.act(delta);
         stage.draw();
