71d6090272b37e4b65f94e14dd6448a13894ed7b
filip <exedrospl@gmail.com>
2024-05-13 21:43:47 +0200

    deleted unnecessary code in GameScreen

--- core/src/com/gdx/gnomearena/Screens/GameScreen.java
+++ core/src/com/gdx/gnomearena/Screens/GameScreen.java
@@ -123,32 +123,6 @@ public class GameScreen implements Screen {
         bm2image.setPosition(700, -300);
         bm2image.setOrigin(bm2image.getWidth()/2, bm2image.getHeight()/2);
 
-        //TESTING
-
-        BitmapFont bigFont;
-        FreeTypeFontGenerator ftfp;
-        ftfp = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Bebas-Regular.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        param.size = 100;
-        param.color.set(0,0,0,1);
-        bigFont = ftfp.generateFont(param);
-        Label.LabelStyle hudStyle = new Label.LabelStyle();
-        hudStyle.font = bigFont;
-        hudStyle.fontColor = Color.WHITE;
-        table = new Table();
-        scoreLabel = new Label(String.format("%03d",0),hudStyle);
-        hpLabel = new Label(String.format("%03d",0),hudStyle);
-        timeLabel = new Label(String.format("%03d",0),hudStyle);
-        table.add(new Label("SCORE:",hudStyle)).expandX().padRight(30);
-        table.add(scoreLabel);
-        table.row();
-        table.add(new Label("HP:",hudStyle)).expandX().padRight(30);
-        table.add(hpLabel);
-        table.row();
-        table.add(new Label("TIME:",hudStyle)).expandX().padRight(30);
-        table.add(timeLabel);
-        table.setPosition(1100,700);
-        table.setWidth(200);
 
     }
 
