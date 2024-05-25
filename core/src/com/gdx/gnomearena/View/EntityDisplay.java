91c6f2c83bfe67af3b6d698c81442c0361c5e9e8
Bbyz <bbyzdev@gmail.com>
2024-05-25 20:42:44 +0200

    Added health display to damaged entities

--- core/src/com/gdx/gnomearena/View/EntityDisplay.java
+++ core/src/com/gdx/gnomearena/View/EntityDisplay.java
@@ -2,19 +2,57 @@ package com.gdx.gnomearena.View;
 
 import java.util.List;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.gdx.gnomearena.Model.Entity;
 import com.gdx.gnomearena.Model.Pair;
 
 public class EntityDisplay extends GameBoardDisplay
 {
     private float timeFromPreviousMove = 0;
+    private final float entityHealthPadding = 50f;
+    private final float entityHealthScale = 1f;
 
     public EntityDisplay()
     {
 
+    }
+
+    private void showEntitiesHealth(Entity entity, float xPos, float yPos, Stage stage) {
+        int maxHealth = entity.maxHealth;
+        int currentHealth = entity.health;
+
+        if(maxHealth==currentHealth)
+        {
+            return;
+        }
+
+        float heartWidth = 32;
+        float heartHeight = 32;
+        float padding = 1;
+
+        Table table = new Table();
+
+        for (int i = 0; i < currentHealth; i++) {
+            Image heartFull = new Image(new Texture("otherSprites/HeartFull.png"));
+            heartFull.setScale(entityHealthScale);
+            table.add(heartFull).width(heartWidth).height(heartHeight).padRight(padding);
+        }
+
+
+        for (int i = currentHealth; i < maxHealth; i++) {
+            Image heartEmpty = new Image(new Texture("otherSprites/HeartEmpty.png"));
+            heartEmpty.setScale(entityHealthScale);
+            table.add(heartEmpty).width(heartWidth).height(heartHeight).padRight(padding);
+        }
+
+        table.setPosition(xPos, yPos + entityHealthPadding);
+        table.setScale(1.5f*totalBoardScale);
+        //table.pack();
+        stage.addActor(table);
     }
 
     private Pair<Float,Float> calculateEntitiesPosition(Pair<Integer,Integer> oldPos, Pair<Integer,Integer> newPos)
@@ -42,9 +80,12 @@ public class EntityDisplay extends GameBoardDisplay
             Image im = new Image(spr);
             im.setOrigin(im.getWidth()/2, im.getHeight()/2);
             Pair<Float,Float> imagePos = calculateEntitiesPosition(entities.get(i).getValue().getValue(), entities.get(i).getValue().getKey());
-            im.setPosition(getEntitiesXPosition(imagePos.getKey(), im.getWidth()), getEntitiesYPosition(imagePos.getValue(), im.getHeight()));
+            float imX = getEntitiesXPosition(imagePos.getKey(), im.getWidth());
+            float imY = getEntitiesYPosition(imagePos.getValue(), im.getHeight());
+            im.setPosition(imX, imY);
             im.setScale(1.5f*gnomeScale*totalBoardScale);
             stage.addActor(im);
+            showEntitiesHealth(entities.get(i).getKey(), imX + im.getWidth()*totalBoardScale/2, imY, stage);
         }
     }
 }
