b6505a2e89fc862a9f10c8ba07e8111dad46a48d
filip <exedrospl@gmail.com>
2024-05-27 03:59:34 +0200

    added sound assets and SoundsPlayer class

---
+++ core/src/com/gdx/gnomearena/View/SoundsPlayer.java
@@ -0,0 +1,45 @@
+package com.gdx.gnomearena.View;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundsPlayer {
+    Music music;
+    Float soundEffectsVolume;
+    Float musicVolume;
+    public SoundsPlayer()
+    {
+        //music = Gdx.audio.newMusic(Gdx.files.internal("butong.wav"));
+        soundEffectsVolume = 1.0f;
+        musicVolume = 1.0f;
+    }
+
+    public void playMusic()
+    {
+        music.setVolume(musicVolume);
+        music.setLooping(true);
+        music.play();
+    }
+    public void setMusic(Music m)
+    {
+        music = m;
+    }
+
+    public void setSoundEffectsVolume(Float volume)
+    {
+        soundEffectsVolume = volume;
+    }
+
+    public void setMusicVolume(Float volume)
+    {
+        musicVolume = volume;
+    }
+
+    public void playSoundEffect(Sound s)
+    {
+        long id = s.play(soundEffectsVolume);
+        s.setLooping(id,false);
+    }
+
+}
